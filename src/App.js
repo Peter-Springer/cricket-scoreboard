b4a9195d1c2cad723fde36e082e0a406b939d9d2
Peter Springer <peterspringer829@gmail.com>
2017-05-31 14:25:07 -0600

    add reset file and create markup and css for marking a hit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import './reset.css';
 import './App.css';
 
 class App extends Component {
 constructor(props) {
   super(props);
   this.state = {
-    cricket: {numbers: [20, 19, 18, 17, 16, 15, 'B']}
+    count: 1
   };
 }
 
@@ -20,6 +21,15 @@ constructor(props) {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div className='hit-container'>
+          <span>/</span>
+        </div>
+        <div className='hit-container'>
+          <span>X</span>
+        </div>
+        <div className='closed-container'>
+          <span>X</span>
+        </div>
       </div>
     );
   }
