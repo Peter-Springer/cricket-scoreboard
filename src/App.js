7d6509f7cc3ae8ac1baba4e0cd3575e1d16d05c8
Peter Springer <peterspringer829@gmail.com>
2017-05-31 16:31:22 -0600

    basic funcationality for a single row

--- src/App.js
+++ src/App.js
@@ -2,14 +2,9 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './reset.css';
 import './App.css';
+import Scoreboard from './Scoreboard';
 
 class App extends Component {
-constructor(props) {
-  super(props);
-  this.state = {
-    count: 1
-  };
-}
 
   render() {
     return (
@@ -18,17 +13,12 @@ constructor(props) {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Cricket Scoreboard</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <div className='hit-container'>
-          <span>/</span>
-        </div>
-        <div className='hit-container'>
-          <span>X</span>
-        </div>
-        <div className='closed-container'>
-          <span>X</span>
+        <div className='scoreboard-row'>
+            <Scoreboard/>
+          <div>
+            <p className='row-number'>15</p>
+          </div>
+            <Scoreboard/>
         </div>
       </div>
     );
@@ -36,3 +26,16 @@ constructor(props) {
 }
 
 export default App;
+
+// this.state = {
+//   rows: Array.from({ length: 13 }).map((e, i) => i + 1).reverse
+// }
+// buildBoards() {
+//   return this.state.rows.map(e => (
+//         <Scoreboard/>
+//           <div>
+//             <p className='row-number'>e</p>
+//           </div>
+//   ))
+// }
+// const ROW_NUMBER = 13;
